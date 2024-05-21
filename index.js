b3a55725ea7e400dc994d76cad60256695c7f635
Paradoxrc <dinithminindu02@gmail.com>
2024-05-21 15:20:42 +0530

    commit4

--- index.js
+++ index.js
@@ -31,6 +31,48 @@ app.get("/blogs",async(req,res)=>{
     }
 });
 
+app.post("/projects", async (req, res) => {
+    console.log(req.body);
+    // res.send("Creating a project");
+  
+    const project = new Project(req.body);
+  
+    try {
+      const newProject = await project.save();
+      res.status(201).json(newProject);
+    } catch (err) {
+      res.status(400).json({ message: err.message });
+    }
+  });
+
+  app.patch("/projects/:id", async (req, res) => {
+    try {
+      const project = await Project.findById(req.params.id);
+      if (project) {
+        project.set(req.body);
+        const updatedProject = await project.save();
+        res.json(updatedProject);
+      } else {
+        res.status(404).json({ message: "Project not found" });
+      }
+    } catch (err) {
+      res.status(404).json({ message: err.message });
+    }
+  });
+
+  app.delete("/projects/:id", async (req, res) => {
+    try {
+      const project = await Project.findByIdAndDelete(req.params.id);
+      if (project) {
+        res.json({ message: "Project deleted" });
+      } else {
+        res.status(404).json({ message: "Project not found" });
+      }
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
+  });
+
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}/`);
 });
